a325ea57679fce423d0ad536a169bd64c665372c
Connor Tumbleson <connor.tumbleson@gmail.com>
2020-04-26 08:47:33 -0400

    fix: add interface bind for concrete binary reader

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@ declare(strict_types = 1);
 
 namespace App\Providers;
 
+use App\Services\IO\BinaryReader;
+use App\Services\IO\BinaryReaderInterface;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -14,6 +16,11 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
-        //
+        $this->bindServices();
+    }
+
+    private function bindServices(): void
+    {
+        $this->app->bind(BinaryReaderInterface::class, BinaryReader::class);
     }
 }
