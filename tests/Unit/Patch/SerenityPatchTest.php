fe2f0be52e096fe1340e3f41e39a895a936c5c02
Connor Tumbleson <connor.tumbleson@gmail.com>
2020-04-26 09:28:22 -0400

    test: more tests for robust magic check for serenity patches

--- tests/Unit/Patch/SerenityPatchTest.php
+++ tests/Unit/Patch/SerenityPatchTest.php
@@ -3,23 +3,56 @@ declare(strict_types = 1);
 
 namespace Tests\Unit\Patch;
 
+use App\Enums\Patch;
 use App\Services\Patch\Serenity\SerenityPatch;
 use Tests\TestCase;
-use Tests\Traits\HasSerenityPatch;
+use Tests\Traits\HasPatchFile;
 
 class SerenityPatchTest extends TestCase
 {
-    use HasSerenityPatch;
+    use HasPatchFile;
 
-    public function testMagicParsing(): void
+    /** @dataProvider magicDataProvider */
+    public function testMagicParsing(string $filename, Patch $patch, bool $isSerenity): void
     {
         // Arrange
-        $file = $this->getSerenityPatch('1337meteors.serenity');
+        $file = $this->getPatch($filename, $patch);
 
         // Act
-        $isSerenity = SerenityPatch::isValid($file);
+        $isValid = SerenityPatch::isValid($file);
 
         // Assert
-        $this->assertTrue($isSerenity, 'Serenity patch (1337meteors) did not validate as .serenity.');
+        $this->assertEquals($isSerenity, $isValid, "{$filename} did not validate as a .serenity file.");
+    }
+
+    public function magicDataProvider(): array
+    {
+        return [
+            [
+                'filename'    => '1337meteors.serenity',
+                'type'        => Patch::SERENITY(),
+                'is_serenity' => true,
+            ],
+            [
+                'filename' => 'DeadInTheWater.serenity',
+                'type' => Patch::SERENITY(),
+                'is_serenity' => true,
+            ],
+            [
+                'filename' => 'Delphia.serenity',
+                'type' => Patch::SERENITY(),
+                'is_serenity' => true,
+            ],
+            [
+                'filename' => 'yaymoddies.ppf',
+                'type' => Patch::PPF(),
+                'is_serenity' => false,
+            ],
+            [
+                'filename' => 'a_rip_in_time.sppf',
+                'type' => Patch::SPPF(),
+                'is_serenity' => false
+            ],
+        ];
     }
 }
