cfae2b175b04ead9a66ca97e048d723a24b8d079
Connor Tumbleson <connor.tumbleson@gmail.com>
2020-04-26 18:48:55 -0400

    style: add helper docblocks on Endianness enum

--- app/Enums/Endianness.php
+++ app/Enums/Endianness.php
@@ -5,6 +5,12 @@ namespace App\Enums;
 
 use BenSampo\Enum\Enum;
 
+/**
+ * Class Endianness
+ * @package App\Enums
+ * @method static static LITTLE()
+ * @method static static BIG()
+ */
 class Endianness extends Enum
 {
     public const LITTLE = 'little';
