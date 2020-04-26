8bae1ca4937c78131190b11086101d199924b968
Connor Tumbleson <connor.tumbleson@gmail.com>
2020-04-26 17:16:04 -0400

    feat: enum for game (just h2 for now)

---
+++ app/Enums/Game.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types = 1);
+
+namespace App\Enums;
+
+use BenSampo\Enum\Enum;
+
+/**
+ * Class Game
+ * @package App\Enums
+ * @method static static HALO_2()
+ */
+class Game extends Enum
+{
+    const HALO_2 = 1;
+
+}
