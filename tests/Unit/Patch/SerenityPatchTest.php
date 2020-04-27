1e1d1b5b61b509bba2d0f5dcacfdc31ead64e869
Connor Tumbleson <connor.tumbleson@gmail.com>
2020-04-27 08:35:23 -0400

    test: add extraction for serenity patch

--- tests/Unit/Patch/SerenityPatchTest.php
+++ tests/Unit/Patch/SerenityPatchTest.php
@@ -5,6 +5,7 @@ namespace Tests\Unit\Patch;
 
 use App\Enums\Patch;
 use App\Services\Patch\Serenity\SerenityPatch;
+use Illuminate\Support\Arr;
 use Tests\TestCase;
 use Tests\Traits\HasPatchFile;
 
@@ -12,17 +13,99 @@ class SerenityPatchTest extends TestCase
 {
     use HasPatchFile;
 
-    /** @dataProvider magicDataProvider */
+    /**
+     * @param string $filename
+     * @param Patch $patch
+     * @param bool $isSerenity
+     * @dataProvider magicDataProvider
+     */
     public function testMagicParsing(string $filename, Patch $patch, bool $isSerenity): void
     {
         // Arrange
         $file = $this->getPatch($filename, $patch);
 
         // Act
-        $isValid = SerenityPatch::isValid($file);
+        $isValid = SerenityPatch::isMagicValid($file);
 
         // Assert
         $this->assertEquals($isSerenity, $isValid, "{$filename} did not validate as a .serenity file.");
+    }
+
+    /**
+     * @param string $filename
+     * @param Patch $patch
+     * @param array $fields
+     * @dataProvider fieldExtractionDataProvider
+     */
+    public function testParsingFileFormat(string $filename, Patch $patch, array $fields): void
+    {
+        // Arrange
+        $file = $this->getPatch($filename, $patch);
+
+        // Act
+        $serenityPatch = new SerenityPatch();
+        $serenityPatch->extractPatchInfo($file);
+        $modImage = $serenityPatch->getModImage();
+
+        // Assert
+        $this->assertEquals(Arr::get($fields, 'author'), $serenityPatch->getAuthor());
+        $this->assertEquals(Arr::get($fields, 'map_name'), $serenityPatch->getMapName());
+        $this->assertEquals(Arr::get($fields, 'mod_name'), $serenityPatch->getModMapName());
+        $this->assertEquals(Arr::get($fields, 'mod_title'), $serenityPatch->getModName());
+        $this->assertEquals(Arr::get($fields, 'description'), $serenityPatch->getModDescription());
+        $this->assertEquals(Arr::get($fields, 'image.width'), $modImage->width());
+        $this->assertEquals(Arr::get($fields, 'image.height'), $modImage->height());
+    }
+
+    public function fieldExtractionDataProvider(): array
+    {
+        return [
+            [
+                'filename' => '1337meteors.serenity',
+                'type'     => Patch::SERENITY(),
+                'fields'   => [
+                    'author'      => 'TheTyckoMan',
+                    'map_name'    => 'ascension.map',
+                    'mod_name'    => '1337 Meteors.map',
+                    'mod_title'   => '1337 Meteors',
+                    'description' => 'Avoid the pesky kill-taking meteors...this is the life of the people who play 1337 meteors...',
+                    'image'       => [
+                        'width'  => 224,
+                        'height' => 207,
+                    ],
+                ],
+            ],
+            [
+                'filename' => 'DeadInTheWater.serenity',
+                'type'     => Patch::SERENITY(),
+                'fields'   => [
+                    'author'      => 'pyroman31',
+                    'map_name'    => 'ascension.map',
+                    'mod_name'    => 'deadinthewater.map',
+                    'mod_title'   => 'Dead in the Water',
+                    'description' => 'You have been left on an island for dead. Escape and you will be honored, but die and you will join the others. Can you survive?',
+                    'image'       => [
+                        'width'  => 224,
+                        'height' => 207,
+                    ],
+                ],
+            ],
+            [
+                'filename' => 'Delphia.serenity',
+                'type'     => Patch::SERENITY(),
+                'fields'   => [
+                    'author'      => 'Boranes',
+                    'map_name'    => 'lockout.map',
+                    'mod_name'    => 'delphia.map',
+                    'mod_title'   => 'Delphia',
+                    'description' => 'With Halo destroyed, the ancient flood research facility is now an over-growth of plant life and insects.',
+                    'image'       => [
+                        'width'  => 224,
+                        'height' => 207,
+                    ],
+                ],
+            ],
+        ];
     }
 
     public function magicDataProvider(): array
