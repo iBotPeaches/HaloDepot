68186086aadcb5e063aeea9a9ea176ff24770c79
Connor Tumbleson <connor.tumbleson@gmail.com>
2020-04-29 22:36:36 -0400

    feat: add support for new clean_name/custom data

---
+++ database/migrations/2020_04_30_021755_add_clean_name_and_custom_data_to_patches.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddCleanNameAndCustomDataToPatches extends Migration
+{
+    public function up(): void
+    {
+        Schema::table('patches', function (Blueprint $table) {
+            $table->string('map', 128)->after('name');
+            $table->json('data')->after('views');
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('patches', function (Blueprint $table) {
+            $table->dropColumn('map');
+            $table->dropColumn('data');
+        });
+    }
+}
