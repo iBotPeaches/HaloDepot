e243db79f0401f6b1722d4a95b7c947a59ef8f36
Connor Tumbleson <connor.tumbleson@gmail.com>
2020-04-30 06:32:23 -0400

    fix: register patch observer with provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@ declare(strict_types = 1);
 
 namespace App\Providers;
 
+use App\Observers\PatchObserver;
+use App\Patch;
 use App\Services\IO\BinaryReader;
 use App\Services\IO\BinaryReaderInterface;
 use Illuminate\Support\ServiceProvider;
@@ -17,6 +19,12 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         $this->bindServices();
+        $this->bindObservers();
+    }
+
+    private function bindObservers(): void
+    {
+        Patch::observe(PatchObserver::class);
     }
 
     private function bindServices(): void
