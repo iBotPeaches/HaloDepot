1d324ae1c3a8b013cdd04aa9c130fac6ecf50761
Connor Tumbleson <connor.tumbleson@gmail.com>
2020-04-30 07:34:16 -0400

    feat: only load onChange behavior if form found

--- resources/js/partials/upload.js
+++ resources/js/partials/upload.js
@@ -1,9 +1,11 @@
 window.addEventListener('DOMContentLoaded', () => {
     const fileInput = document.querySelector('#patch-file-upload input[type=file]');
-    fileInput.onchange = () => {
-        if (fileInput.files.length > 0) {
-            const fileName = document.querySelector('#patch-file-upload .file-name');
-            fileName.textContent = fileInput.files[0].name;
+    if (fileInput) {
+        fileInput.onchange = () => {
+            if (fileInput.files.length > 0) {
+                const fileName = document.querySelector('#patch-file-upload .file-name');
+                fileName.textContent = fileInput.files[0].name;
+            }
         }
     }
 });
