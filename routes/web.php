fb98be4b6ac09024aab8c08ccc207753435f63ea
Connor Tumbleson <connor.tumbleson@gmail.com>
2020-05-01 07:10:15 -0400

    feat: new web route for patch view/download

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,7 @@ Route::get('/', 'HomeController@index')->name('home');
 
 Route::get('/upload', 'UploadController@show')->name('upload.show');
 Route::post('/upload', 'UploadController@store')->name('upload.store');
+
+// Patches
+Route::get('/patch/{patch}', 'PatchController@show')->name('patch.show');
+Route::get('/patch/{patch}/download', 'PatchController@download')->name('patch.download');
