30a26c6103610ef58aaf95a2702f34264cea570d
Connor Tumbleson <connor.tumbleson@gmail.com>
2020-05-01 07:10:51 -0400

    fix: redirect to patch after upload

--- app/Http/Controllers/UploadController.php
+++ app/Http/Controllers/UploadController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 
 use App\Factories\PatchFactory;
 use App\Http\Requests\UploadPatchRequest;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\View\View;
@@ -16,13 +17,12 @@ class UploadController extends Controller
         return view('pages/upload');
     }
 
-    public function store(UploadPatchRequest $request): View
+    public function store(UploadPatchRequest $request): RedirectResponse
     {
         return DB::transaction(function() use ($request) {
             $patch = PatchFactory::identifyAddPatch($request->file('patch'));
 
-            // TODO - Redirect to patch
-            return view('pages/upload');
+            return redirect()->route('patch.show', [$patch]);
         });
     }
 }
