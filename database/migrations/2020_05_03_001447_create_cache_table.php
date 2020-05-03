0f5e9e754d3db31c0ef23e8533adf4c2673326e0
Connor Tumbleson <connor.tumbleson@gmail.com>
2020-05-02 20:15:31 -0400

    feat: add session/cache into db

---
+++ database/migrations/2020_05_03_001447_create_cache_table.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCacheTable extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('cache', function (Blueprint $table) {
+            $table->string('key')->unique();
+            $table->mediumText('value');
+            $table->integer('expiration');
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('cache');
+    }
+}
